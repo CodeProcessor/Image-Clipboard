a7f10be9b91f38ebf47eabcb0daab2993be22a4f
Dulan Jayasuriya <dulanjayasuriya@gmail.com>
2021-09-19 14:26:36 +0530

    Improved the code Copy to clipboard

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Image-Clipboard
+
+
+## How to install
+
+```
+sudo apt-get update
+sudo apt-get install libleptonica-dev 
+sudo apt-get install tesseract-ocr tesseract-ocr-dev
+sudo apt-get install libtesseract-dev
+sudo apt-get install tesseract-ocr
+
+pip3 install pytesseract pynput pyscreenshot
+```
--- main.py
+++ main.py
@@ -6,56 +6,45 @@ Created on 3/28/2020
 from pynput.mouse import Listener, Button
 import pyscreenshot as ImageGrab
 import pytesseract
-from PIL import Image
+import pyperclip
 
 
 class Main():
-    def __init__(self):
-        self.stage_1 = False
+    def __init__(self, save_image=False):
+        self.press_coordinates_captured = False
         self.press_coords = None
         self.release_coords = None
+        self.save_image = save_image
 
-        with Listener(on_click=self.on_click, on_scroll=self.on_scroll) as listener:
+        with Listener(on_click=self.on_click) as listener:
             listener.join()
 
     def on_click(self, x, y, button, pressed):
-
-        if button == Button.left:
-            stage_1 = False
-
+        if button == Button.middle:
             if pressed:
-                print("pressed")
-                if not self.stage_1:
+                if not self.press_coordinates_captured:
                     self.press_coords = (x, y)
-                    self.stage_1 = True
-                    print("Stage 1 OK!")
-                else:
+                    self.press_coordinates_captured = True
+            else:
+                if self.press_coordinates_captured:
                     self.release_coords = (x, y)
                     try:
                         im = ImageGrab.grab(bbox=(
                         self.press_coords[0], self.press_coords[1], self.release_coords[0], self.release_coords[1]))
-                        filename = "somename_{}.png".format(self.press_coords[0])
+                        filename = ".temp.jpg"
+                        if self.save_image:
+                            filename = "image_clipped_{}.png".format(self.press_coords[0])
                         im.save(filename)
                         text = pytesseract.image_to_string(im)
-                        print("Image saved, workds: {}".format(text))
-                        self.stage_1 = False
-                    except ValueError:
+                        pyperclip.copy(text)
+                        print("Image saved, words: {}".format(text))
+                    except ValueError as e:
+                        print(e)
                         print("Check the click!!!")
-
-        if button == Button.right:
-            self.stage_1 = False
-            print("Reset")
-
-        #                                   This is a test
-        print(button)
-
-    def on_move(self, x, y):
-        print ("Mouse moved")
-
-
-    def on_scroll(self, x, y, dx, dy):
-        print ("Mouse scrolled")
-
+                    finally:
+                        self.press_coordinates_captured = False
+        else:
+            self.press_coordinates_captured = False
 
 
 if __name__ == "__main__":
